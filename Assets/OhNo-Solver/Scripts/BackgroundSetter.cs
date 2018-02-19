89f6f9419c8ed4159d18da0c18ddb4e64f3184ee
jdpoenisch <jerome.gandalf@web.de>
2018-02-19 19:40:53 +0100

    Added Input Coloring

---
+++ Assets/OhNo-Solver/Scripts/BackgroundSetter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundSetter : MonoBehaviour
+{
+
+    private Image image;
+
+    private InputField inputField;
+
+    // Use this for initialization
+    void Start()
+    {
+        image = GetComponent<Image>();
+        inputField = GetComponentInChildren<InputField>();
+    }
+
+    public void SetBackground()
+    {
+        switch (inputField.text)
+        {
+            case "":
+                image.color = Color.white;
+                break;
+
+            case "r":
+                image.color = Color.red;
+                break;
+
+            default:
+                int x = 0;
+                int.TryParse(inputField.text, out x);
+                image.color = x == 0 ? Color.magenta : Color.blue;
+                break;
+        }
+    }
+}
